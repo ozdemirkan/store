8d6cf8490767ca2b32054472ae44f279338dd8ca
ozdemirkan <ozdemirkan@gmail.com>
2021-07-19 13:01:37 +0300

    inital template

---
+++ store-product-service/src/test/java/com/ozdemirkan/store/product/ProductApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ozdemirkan.store.product;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
